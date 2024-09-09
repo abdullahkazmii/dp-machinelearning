049f441b832ae5a7b4fb2e681ea56b68d6edb1df
Abdullah Kazmi <112763322+abdullahkazmii@users.noreply.github.com>
2024-09-09 13:25:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('🤖 Machine Learning App')
 st.info("Abdullah's first Machine Learning App!")
 
 with st.expander("Data"):
-  st.write("Raw Data:")
+  st.write("**Raw Data:***")
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
