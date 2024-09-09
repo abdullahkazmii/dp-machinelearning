57ac3787ecfc44b11fbdb7dfd0c7a52155a222e0
Abdullah Kazmi <112763322+abdullahkazmii@users.noreply.github.com>
2024-09-09 18:36:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,6 +88,30 @@ df_prediction_probab.rename(columns = {
 
 #Display Predicted Species
 st.subheader('Predicted Species')
+st.dataframe(df_prediction_probab,
+            column_config = {
+              'Adelie': st.column_config.ProgressColumn(
+                'Adelie',
+                format = '%f', 
+                width: 'medium',
+                min_value=0,
+                max_value=1
+              ),
+              'Chinstrap': st.column_config.ProgressColumn(
+                'Chinstrap',
+                format = '%f', 
+                width: 'medium',
+                min_value=0,
+                max_value=1
+              ),
+              'Gentoo': st.column_config.ProgressColumn(
+                'Gentoo',
+                format = '%f', 
+                width: 'medium',
+                min_value=0,
+                max_value=1
+              ),
+            },   )
 df_prediction_probab
 
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
