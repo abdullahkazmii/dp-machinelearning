159c7d9c2888246ce669873ac7469a4136f4cf54
Abdullah Kazmi <112763322+abdullahkazmii@users.noreply.github.com>
2024-09-09 13:15:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info("Abdullah's first Machine Learning App!")
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
