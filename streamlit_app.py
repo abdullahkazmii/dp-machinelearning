bacab684ca977f993691b0e348cc34f1844845bf
Abdullah Kazmi <112763322+abdullahkazmii@users.noreply.github.com>
2024-09-09 13:51:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,6 @@ with st.expander("Data"):
   Y = df[['species', 'sex']]
   Y
 
+with st.expander("Data Visualization"):
+  st.scatter_chart(data = df, x='bill_length_mm', y = 'body_mass_g', color = 'species')
+  
